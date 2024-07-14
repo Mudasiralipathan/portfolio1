3fd93bbe507801ab43c728ce21b213f5a432baa9
mudasir <mapppali1108@gmail.com>
2024-07-13 22:58:12 -0700

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Header.css'
-import headerimg from "../../images/ninja-logo.png"
+import headerimg from "./images/ninja-logo.png"
 
 const Header = () => {
     const nav =[
